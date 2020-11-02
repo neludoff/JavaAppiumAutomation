1cf934c78b054cb3290703ee7892e61d6943243c
Vasily <neludoff@gmail.com>
2020-11-02 17:48:55 +0300

    Test 3
    + update for test 1

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,9 +1,14 @@
 public class MainClass{
 
     private int class_number = 20;
+    private String class_string = "Hello, world";
 
     public int getClassNumber(){
         return this.class_number;
+    }
+
+    public String getClassString(){
+        return this.class_string;
     }
     
     public int getLocalNumber()
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,11 +6,17 @@ public class MainClassTest extends MainClass{
     @Test
     public void CheckLocalNumber()
     {
-        Assert.assertTrue("Return number is 14", this.getLocalNumber() == 14);
+        Assert.assertEquals("Return number is 14", 14, this.getLocalNumber());
     }
 
     @Test
     public void testGetClassNumber(){
         Assert.assertTrue("Returned value greater than 45", this.getClassNumber() > 45);
     }
+
+    @Test
+    public void testGetClassString(){
+        Assert.assertTrue("String contains rather 'Hello' or 'hello' words", this.getClassString().toLowerCase().contains("hello"));
+    }
+
 }
