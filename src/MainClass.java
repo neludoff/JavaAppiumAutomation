6a8de846a1e93bc462774d0237d0323b130d1b85
Vasily <neludoff@gmail.com>
2020-11-02 17:10:37 +0300

    Test 2
    Home work. Test 2

--- src/MainClass.java
+++ src/MainClass.java
@@ -1,5 +1,10 @@
 public class MainClass{
 
+    public int getClassNumber(){
+        int class_number = 20;
+        return class_number;
+    }
+    
     public int getLocalNumber()
     {
         return 14;
--- src/MainClassTest.java
+++ src/MainClassTest.java
@@ -6,6 +6,11 @@ public class MainClassTest extends MainClass{
     @Test
     public void CheckLocalNumber()
     {
-        Assert.assertTrue("Return number is 14", this.getLocalNumber() == 15);
+        Assert.assertTrue("Return number is 14", this.getLocalNumber() == 14);
+    }
+
+    @Test
+    public void testGetClassNumber(){
+        Assert.assertTrue("Returned value greater than 45", this.getClassNumber() > 45);
     }
 }
